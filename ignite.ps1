d5658f8d74359ff26c9a8cd6f0739b9976518b4f
N3tb0ss <bscalan@gmail.com>
2017-07-02 14:14:02 -0400

    Trying to use @hash()

--- ignite.ps1
+++ ignite.ps1
@@ -30,21 +30,48 @@ $ovfConfig.NetworkMapping.GigabitEthernet1.Value = $network1
 $ovfConfig.NetworkMapping.GigabitEthernet2.Value = $network2
 $ovfConfig.NetworkMapping.GigabitEthernet3.Value = $network3
 $ovfConfig.DeploymentOption.Value = $DeploymentSize
-$ovfConfig.com.cisco.csr1000v.1.hostname.Value = $VMName
-$ovfConfig.com.cisco.csr1000v.1.login_username.Value = "admin"
-$ovfConfig.com.cisco.csr1000v.1.login_password.Value = "VMware1!"
-$ovfConfig.com.cisco.csr1000v.1.mgmt_interface.Value = "GigabitEthernet1"
-$ovfConfig.com.cisco.csr1000v.1.mgmt_vlan.Value = "10"
-$ovfConfig.com.cisco.csr1000v.1.mgmt_ipv4_addr.Value = "10.10.10.237/24"
-$ovfConfig.com.cisco.csr1000v.1.mgmt_ipv4_gateway.Value = "10.10.10.1"
-$ovfConfig.com.cisco.csr1000v.1.mgmt_network.Value = "10.10.10.0/24"
-$ovfConfig.com.cisco.csr1000v.1.enable_scp_server.Value = "True"
-$ovfConfig.com.cisco.csr1000v.1.enable_ssh_server.Value = "True"
-$ovfConfig.com.cisco.csr1000v.1.privilege_password.Value = "VMware1!"
-$ovfConfig.com.cisco.csr1000v.1.domain_name.Value = "fatpacket.lab"
-$ovfConfig.com.cisco.csr1000v.1.license.Value = "ax"
-$ovfConfig.com.cisco.csr1000v.1.resource_template.Value = "default"
+$ovfConfig.com.cisco.csr1000v.'1'.hostname.Value = $VMName
+$ovfConfig.com.cisco.csr1000v.'1'.login_username.Value = "admin"
+$ovfConfig.com.cisco.csr1000v.'1'.login_password.Value = "VMware1!"
+$ovfConfig.com.cisco.csr1000v.'1'.mgmt_interface.Value = "GigabitEthernet1"
+$ovfConfig.com.cisco.csr1000v.'1'.mgmt_vlan.Value = "10"
+$ovfConfig.com.cisco.csr1000v.'1'.mgmt_ipv4_addr.Value = "10.10.10.237/24"
+$ovfConfig.com.cisco.csr1000v.'1'.mgmt_ipv4_gateway.Value = "10.10.10.1"
+$ovfConfig.com.cisco.csr1000v.'1'.mgmt_ipv4_network.Value = "10.10.10.0/24"
+$ovfConfig.com.cisco.csr1000v.'1'.enable_scp_server.Value = "True"
+$ovfConfig.com.cisco.csr1000v.'1'.enable_ssh_server.Value = "True"
+$ovfConfig.com.cisco.csr1000v.'1'.privilege_password.Value = "VMware1!"
+$ovfConfig.com.cisco.csr1000v.'1'.domain_name.Value = "fatpacket.lab"
+$ovfConfig.com.cisco.csr1000v.'1'.license.Value = "ax"
+$ovfConfig.com.cisco.csr1000v.'1'.resource_template.Value = "default"
+$ovfConfig.com.cisco.csr1000v.'1'.ios-config-0001.Value = "aaa new-model"
 
 
-$vm = Import-VApp -Server $pEsxi -Source $NestedESXiApplianceOVA -OvfConfiguration $ovfconfig -Name $VMName -VMHost $vmhost -Datastore $datastore -DiskStorageFormat thin
+$vm = Import-VApp -Server $pEsxi -Source $NestedCSRApplianceOVA -OvfConfiguration $ovfconfig -Name $VMName -VMHost $vmhost -Datastore $datastore -DiskStorageFormat thin
             
+
+$hash = @{
+"NetworkMapping.GigabitEthernet1" = $network1
+"NetworkMapping.GigabitEthernet2" = $network2
+"NetworkMapping.GigabitEthernet3" = $network3
+"DeploymentOption" = $DeploymentSize
+"com.cisco.csr1000v.'1'.hostname" = "CSR1000v-2"
+"com.cisco.csr1000v.'1'.console" = "auto"
+"com.cisco.csr1000v.'1'.domain-name" = "fatpacket.home"
+"com.cisco.csr1000v.'1'.enable_scp_server" = "false"
+"com.cisco.csr1000v.'1'.enable_ssh_server" = "false"
+"com.cisco.csr1000v.'1'.license" = "ax"
+"com.cisco.csr1000v.'1'.login_password" = "VMware1!"
+"com.cisco.csr1000v.'1'.login_user" = "admin"
+"com.cisco.csr1000v.'1'.mgmt_interface" = "GigabitEthernet1"
+"com.cisco.csr1000v.'1'.mgmt_ipv4_addr" = '10.10.10.237/24'
+"com.cisco.csr1000v.'1'.mgmt_ipv4_gateway" = "10.10.10.1"
+"com.cisco.csr1000v.'1'.mgmt_ipv4_network" = '10.10.10.0/24'
+"com.cisco.csr1000v.'1'.mgmt_vlan" = "10"
+"com.cisco.csr1000v.'1'.privilege_password" = "VMware1!"
+"com.cisco.csr1000v.'1'.ios-config-0001" = "aaa new-model"
+}
+
+
+$vm = Import-VApp -Server $pEsxi -Source $NestedCSRApplianceOVA -OvfConfiguration $hash -Name $VMName -VMHost $vmhost -Datastore $datastore -DiskStorageFormat thin
+ 
